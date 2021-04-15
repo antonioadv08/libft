be2b410424c17694000d89dbaea163943d07108b
antonioadv08 <antonioadv08@gmail.com>
2021-04-16 00:34:23 +0200

    commit

--- ft_memmove.c
+++ ft_memmove.c
@@ -13,32 +13,59 @@
 #include <string.h>
 #include <stdio.h>
 
-void *ft_memmove(void *dest, const void *src, size_t n)
-{
-	size_t i;
+// void *ft_memmove(void *dest, const void *src, size_t n)
+// {
+// 	size_t i;
 
+// 	i = 0;
+// 	while (i < n && ((unsigned char *)src)[i] != '\n')
+// 	{
+// 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+// 		i++;
+// 	}
+// 	return ((char *)dest);
+// }
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*destin;
+	unsigned char	*source;
+	size_t			i;
+
+	destin = (unsigned char *)dest;
+	source = (unsigned char *)src;
+	if (src == NULL && dest == NULL)
+		return (NULL);
 	i = 0;
-	while (i < n && ((unsigned char *)src)[i] != '\n')
+	while (i < n)
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		if (src < dest)
+			destin[n - i - 1] = source[n - i - 1];
+		else
+			destin[i] = source[i];
 		i++;
 	}
-	return ((char *)dest);
+	return (dest);
 }
 
-// int main()
-// {
-// 	char src[] = "lorem ipsum dolor sit amet";
-// 	char *dest;
+int main()
+{
+	char *x;
+	char *y;
 
-// 	dest = src + 1;
-// 	// int number = 8;
+	char dst1[0xF0];
+	char dst2[0xF0];
+	char *data = "thiß ß\xde\xad\xbe\xeftriñg will be øvérlapéd !\r\n";
+	int size = 0xF0 - 0xF;
 
-// 		char *x;
-// 	char *y;
-// 	y = memmove(dest, "con\0sec\0\0te\0tur", 10);
-// 	x = ft_memmove(dest, "con\0sec\0\0te\0tur", 10);
+	memset(dst1, 'A', sizeof(dst1));
+	memset(dst2, 'A', sizeof(dst2));
 
-// 	printf("memmove:    %s \n", y);
-// 	printf("ft_memmove: %s \n", x);
-// }
+	memcpy(dst1, data, strlen(data));
+	memcpy(dst2, data, strlen(data));
+	y = memmove(dst1 + 3, dst1, size);
+
+	x = ft_memmove(dst2 + 3, dst2, size);
+
+	printf("memmove:    %s \n", y);
+	printf("ft_memmove: %s \n", x);
+}
