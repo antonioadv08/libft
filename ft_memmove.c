c2bdaab0615956c299747e15a298b8c674b46791
antonioadv08 <antonioadv08@gmail.com>
2021-04-16 13:10:35 +0200

    commit

--- ft_memmove.c
+++ ft_memmove.c
@@ -47,25 +47,25 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-int main()
-{
-	char *x;
-	char *y;
+// int main()
+// {
+// 	char *x;
+// 	char *y;
 
-	char dst1[0xF0];
-	char dst2[0xF0];
-	char *data = "thiß ß\xde\xad\xbe\xeftriñg will be øvérlapéd !\r\n";
-	int size = 0xF0 - 0xF;
+// 	char dst1[0xF0];
+// 	char dst2[0xF0];
+// 	char *data = "thiß ß\xde\xad\xbe\xeftriñg will be øvérlapéd !\r\n";
+// 	int size = 0xF0 - 0xF;
 
-	memset(dst1, 'A', sizeof(dst1));
-	memset(dst2, 'A', sizeof(dst2));
+// 	memset(dst1, 'A', sizeof(dst1));
+// 	memset(dst2, 'A', sizeof(dst2));
 
-	memcpy(dst1, data, strlen(data));
-	memcpy(dst2, data, strlen(data));
-	y = memmove(dst1 + 3, dst1, size);
+// 	memcpy(dst1, data, strlen(data));
+// 	memcpy(dst2, data, strlen(data));
+// 	y = memmove(dst1 + 3, dst1, size);
 
-	x = ft_memmove(dst2 + 3, dst2, size);
+// 	x = ft_memmove(dst2 + 3, dst2, size);
 
-	printf("memmove:    %s \n", y);
-	printf("ft_memmove: %s \n", x);
-}
+// 	printf("memmove:    %s \n", y);
+// 	printf("ft_memmove: %s \n", x);
+// }
