3bbf3615a988d47b8f86c67364fbb623fb9aff11
antonioadv08 <antonioadv08@gmail.com>
2021-04-21 17:50:48 +0200

    commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,29 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:07:58 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/21 17:49:36 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:50:44 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+// #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
-{
+// char *ft_strtrim(char const *s1, char const *set)
+// {
 
-    char *str;
-    int i;
-    int j;
-    int k;
+//     char *str;
+//     int i;
+//     int j;
+//     int k;
 
-    int s1_len;
+//     int s1_len;
 
-    i = 0;
-    k = 0;
-    j = 0;
-    s1_len = ft_strlen(s1);
-    str = malloc(s1_len);
-    if (str == NULL)
-        return (NULL);
+//     i = 0;
+//     k = 0;
+//     j = 0;
+//     s1_len = ft_strlen(s1);
+//     str = malloc(s1_len);
+//     if (str == NULL)
+//         return (NULL);
     
     
 
@@ -38,8 +38,8 @@ char *ft_strtrim(char const *s1, char const *set)
     
 
 
-    return str;
-}
+//     return str;
+// }
 
 // int main(void)
 // {
