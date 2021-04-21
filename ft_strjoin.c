55b32aa2c979dcec27f70dbf861fb9e348b9baef
antonioadv08 <antonioadv08@gmail.com>
2021-04-21 12:46:10 +0200

    commit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 11:58:47 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/21 12:30:26 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/21 12:46:06 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char *ft_strjoin(char const *s1, char const *s2)
     s2_len = ft_strlen(s2);
     dest = malloc(s1_len + s2_len);
     j = 0;
-    if (s1 == NULL || s2 == NULL)
+    if (s1 == NULL || s2 == NULL || dest == NULL)
         return (NULL);
 
     while (j < s1_len)
@@ -34,15 +34,15 @@ char *ft_strjoin(char const *s1, char const *s2)
     while (j <= s1_len + s2_len)
         dest[j++] = s2[i++];
 
-    dest[s1_len+s2_len]='\0';
+    dest[s1_len + s2_len] = '\0';
     return (dest);
 }
 
-int main(void)
-{
+// int main(void)
+// {
 
-    char *s1 = "hola";
-    char *s2 = "felex";
+//     char *s1 = "hola";
+//     char *s2 = "felex";
 
-    printf("ft_strjoin: %s \n", ft_strjoin(s1, s2));
-}
+//     printf("ft_strjoin: %s \n", ft_strjoin(s1, s2));
+// }
