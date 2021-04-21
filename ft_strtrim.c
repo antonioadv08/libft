ced4022ae6b19e79a22ef35f064f94af1fd36558
antonioadv08 <antonioadv08@gmail.com>
2021-04-21 17:49:40 +0200

    commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:07:58 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/21 17:44:17 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:49:36 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,11 @@ char *ft_strtrim(char const *s1, char const *set)
     return str;
 }
 
-int main(void)
-{
+// int main(void)
+// {
 
-    char *s1 = "hola buenos diasho";
-    char *set = "ho";
+//     char *s1 = "hola buenos diasho";
+//     char *set = "ho";
 
-    printf("ft_strtrim: %s \n", ft_strtrim(s1, set));
-}
+//     printf("ft_strtrim: %s \n", ft_strtrim(s1, set));
+// }
