4aae8d56aee327b7bf801df493089951bc665796
antonioadv08 <antonioadv08@gmail.com>
2021-04-23 01:10:22 +0200

    commit

--- libft.h
+++ libft.h
@@ -6,18 +6,18 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 12:44:57 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/22 19:55:30 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/23 01:09:14 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-# include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct s_list
 {
@@ -56,5 +56,8 @@ void *ft_calloc(size_t nmemb, size_t size);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(char const *s, char c);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int ft_putchar_fd(char c, int fd);
+int ft_putendl_fd(char *s, int fd);
 
 #endif
