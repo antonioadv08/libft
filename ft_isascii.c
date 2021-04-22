b693ce1779cb317fb2e121ccdf1e4523dc857d16
antonioadv08 <antonioadv08@gmail.com>
2021-04-22 15:08:33 +0200

    commit

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 15:20:15 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/21 19:16:21 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/22 15:08:27 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int ft_isascii(int c)
     return (0);
 }
 
-int main()
-{
-    char c;
-    c =128;
-    printf("isascii: %d", isascii(c));
+// int main()
+// {
+//     char c;
+//     c =128;
+//     printf("isascii: %d", isascii(c));
 
-    printf("\nft_isascii: %d \n", ft_isascii(c));
+//     printf("\nft_isascii: %d \n", ft_isascii(c));
 
-    return 0;
-}
+//     return 0;
+// }
