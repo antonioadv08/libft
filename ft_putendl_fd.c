b0ef1efddbd8a114422868acfb6e1d62a4872fa2
antonioadv08 <antonioadv08@gmail.com>
2021-04-23 01:07:03 +0200

    commit

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:07:25 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/23 01:05:42 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/23 01:06:42 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ int ft_putendl_fd(char *s, int fd)
     return write(fd, "\n", 1);
 }
 
-int main(int argc, const char *argv[])
-{
-    int arg;
+// int main(int argc, const char *argv[])
+// {
+//     int arg;
 
-    alarm(5);
-    if (argc == 1)
-        return (0);
-    else if ((arg = atoi(argv[1])) == 1)
-        ft_putendl_fd("lorem ipsum dolor sit amet", 2);
-    else if (arg == 2)
-        ft_putendl_fd("  lorem\nipsum\rdolor\tsit amet  ", 1);
-    else if (arg == 3)
-        ft_putendl_fd("", 2);
-    else if (arg == 4)
-        ft_putendl_fd("lorem ipsum do\0lor sit amet", 1);
-    return (0);
-}
+//     alarm(5);
+//     if (argc == 1)
+//         return (0);
+//     else if ((arg = atoi(argv[1])) == 1)
+//         ft_putendl_fd("lorem ipsum dolor sit amet", 2);
+//     else if (arg == 2)
+//         ft_putendl_fd("  lorem\nipsum\rdolor\tsit amet  ", 1);
+//     else if (arg == 3)
+//         ft_putendl_fd("", 2);
+//     else if (arg == 4)
+//         ft_putendl_fd("lorem ipsum do\0lor sit amet", 1);
+//     return (0);
+// }
