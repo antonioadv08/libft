58c8d46b2954e81f33d58e7d056e2bb68d19ed91
antonioadv08 <antonioadv08@gmail.com>
2021-04-23 15:58:09 +0200

    commit

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,71 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:07:29 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/16 12:07:29 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/23 15:56:46 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft.h"
+
+
+int ft_putnbr_fd(int n, int fd){
+
+
+int	count;
+
+	count = 0;
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		return (ft_putnbr_fd(147483648, fd) + 1);
+	}
+	else if (n > 9 && n <= 2147483647)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+		count += ft_putchar_fd(n + '0', fd);
+	return (count);
+
+}
+
+
+
+
+
+int		main(int argc, const char *argv[])
+{
+	int		arg;
+
+	alarm(5);
+	if (argc == 1)
+		return (0);
+	else if ((arg = atoi(argv[1])) == 1)
+		ft_putnbr_fd(0, 2);
+	else if (arg == 2)
+		ft_putnbr_fd(5, 1);
+	else if (arg == 3)
+		ft_putnbr_fd(-5, 2);
+	else if (arg == 4)
+		ft_putnbr_fd(42, 1);
+	else if (arg == 5)
+		ft_putnbr_fd(-57, 2);
+	else if (arg == 6)
+		ft_putnbr_fd(164189, 1);
+	else if (arg == 7)
+		ft_putnbr_fd(-987441, 2);
+	else if (arg == 8)
+		ft_putnbr_fd(2147483647, 1);
+	else if (arg == 9)
+		ft_putnbr_fd(-2147483648LL, 2);
+	return (0);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,25 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:07:32 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/16 12:07:32 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/04/23 15:33:18 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int ft_putstr_fd(char *s, int fd)
+{
+
+    if (s == NULL)
+        return (0);
+
+    return ((int)write(fd, s, ft_strlen(s)));
+}
+
+int main(void)
+{
+
+    char *s = "hola";
+
+    ft_putstr_fd(s, 1);
+}
