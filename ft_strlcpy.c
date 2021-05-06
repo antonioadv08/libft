a5d81889c737d4a06bbd745dcf897138959b4c5f
antonioadv08 <antonioadv08@gmail.com>
2021-05-07 00:26:44 +0200

    commit

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 15:20:47 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/04/26 14:16:35 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/05/07 00:26:34 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-size_t	ft_strlcpy(char *dest, char *src, unsigned int size)
+size_t	ft_strlcpy(char *dest,const char *src, unsigned int size)
 {
 	unsigned int	count;
 	unsigned int	index;
