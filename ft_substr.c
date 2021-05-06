09c9e7b9374f7ead3fd606a98a88014640a5ac28
antonioadv08 <antonioadv08@gmail.com>
2021-05-07 00:12:48 +0200

    commit

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: adiez-ve <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 12:08:00 by adiez-ve          #+#    #+#             */
-/*   Updated: 2021/05/06 20:17:59 by adiez-ve         ###   ########.fr       */
+/*   Updated: 2021/05/07 00:12:39 by adiez-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 
 	i = start;
 	j = 0;
-	substr = malloc(len + 1);
+	substr = (char *)malloc(len + 1);
 	if (len > ft_strlen(s))
 		len = ft_strlen(s);
 	if (!s || substr == NULL)
